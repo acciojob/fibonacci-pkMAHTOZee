99de3c80ad22a811f477237d89a1d96610ea9483
pkMAHTOZee <86905216+pkMAHTOZee@users.noreply.github.com>
2024-05-14 21:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function fibonacci(num) {
-// your code here
-	let a=0;
-	let b=1;
-	if(num==1)
-		return 0;
-	if(num==2)
-		return 1;
-	let n=3;
-	while(n<=num)
-		{
-			c=a+b;
-			a=b;
-			b=c;
-		}
-	return c;
-	
+    let a = 0;
+    let b = 1;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+    let c;
+    for (let n = 3; n <= num; n++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 }
 
 module.exports = fibonacci;
