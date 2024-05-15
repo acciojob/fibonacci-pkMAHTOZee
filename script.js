2ef01deae84801034d7a3368dd65ee1faaaba347
pkMAHTOZee <86905216+pkMAHTOZee@users.noreply.github.com>
2024-05-15 10:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function fibonacci(num) {
     let a = 0;
     let b = 1;
-    if (num === 1) return 0;
-    if (num === 2) return 1;
+    if (num == 1) return 0;
+    if (num == 2) return 1;
     let c;
     for (let n = 3; n <= num; n++) {
         c = a + b;
